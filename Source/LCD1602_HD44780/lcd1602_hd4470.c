e74fce982d8ab742a5151205db75b59aeb235f03
Nguyen Tan Son <son.nguyentanbk@gmail.com>
2025-10-11 11:16:37 +0700

    [chore] Refactor lcd1602 functions

--- Source/LCD1602_HD44780/lcd1602_hd4470.c
+++ Source/LCD1602_HD44780/lcd1602_hd4470.c
@@ -2,20 +2,62 @@
 #include <stdbool.h>
 #include <stddef.h>
 
-#include "display_config.h"
+#include "./../display_config.h"
 #include "lcd1602_hd4470.h"
 
-#ifdef PLATFORM_TI
+#ifdef TI_CHIPSET
 #include "inc/hw_memmap.h"
 #include "driverlib/gpio.h"
-#endif // PLATFORM_TI
+#endif // TI_CHIPSET
+
+static void lcd_delay_ms(uint32_t ms) 
+{
+    // Implement delay
+}
+
+uint8_t lcd_read(void)
+{
+    
+
+}
+
+void lcd_write(uint16_t data)
+{
+    gpio_write(HD4470_RS_PORT, HD4470_RS_PIN, 0); // Command mode
+    gpio_write(HD4470_RW_PORT, HD4470_RW_PIN, 0); // Write mode
+
+    // Write high nibble
+    gpio_write(HD4470_DATA_7_PORT, HD4470_DATA_7_PIN, data_7(data));
+    gpio_write(HD4470_DATA_6_PORT, HD4470_DATA_6_PIN, data_6(data));
+    gpio_write(HD4470_DATA_5_PORT, HD4470_DATA_5_PIN, data_5(data));
+    gpio_write(HD4470_DATA_4_PORT, HD4470_DATA_4_PIN, data_4(data));
+
+    // Pulse Enable
+    gpio_write(HD4470_E_PORT, HD4470_E_PIN, HD4470_E_PIN);
+    lcd_delay_ms(1);
+    gpio_write(HD4470_E_PORT, HD4470_E_PIN, 0);
+
+    // Write low nibble
+    gpio_write(HD4470_DATA_7_PORT, HD4470_DATA_7_PIN, data_2(data));
+    gpio_write(HD4470_DATA_6_PORT, HD4470_DATA_6_PIN, data_2(data));
+    gpio_write(HD4470_DATA_5_PORT, HD4470_DATA_5_PIN, data_1(data));
+    gpio_write(HD4470_DATA_4_PORT, HD4470_DATA_4_PIN, data_0(data));
+
+    // Pulse Enable
+    gpio_write(HD4470_E_PORT, HD4470_E_PIN, HD4470_E_PIN);
+    lcd_delay_ms(1);
+    gpio_write(HD4470_E_PORT, HD4470_E_PIN, 0);
+
+    lcd_delay_ms(2); // Wait for command to complete
+    
+}
 
 void lcd1602_init(void) 
 {
     // Initialize GPIO pins for RS, RW, E, and data lines
     // Send initialization commands to LCD1602
     lcd_delay_ms(50); // Wait for LCD to power up
-    lcd_write_command(0x38); // Function set: 8-bit, 2 lines, 5x8 dots
+    lcd_write(0x28); // Function set: 4-bit, 2 lines, 5x8 dots
     lcd_delay_ms(5);    // Wait
     lcd_write_command(0x0C); // Display ON, Cursor OFF
     lcd_delay_ms(5);    // Wait
@@ -35,60 +77,4 @@ void lcd1602_clear(void)
 void lcd1602_set_cursor(uint8_t row, uint8_t col) 
 {
 
-}
-
-void lcd1602_write_char(char c) 
-{
-    
-}
-
-void lcd1602_write_string(const char *str) 
-{
-
-}
-
-// Stub implementations for GPIO and delay (replace with your platform-specific code)
-static void lcd_write_data(uint8_t data) 
-{
-    // Implement GPIO write for data
-    write_gpio(GPIO_PORT_DATA, LCD_DATA_PINS, data);
-
-}
-
-static void lcd_write_command(uint8_t cmd) 
-{
-    #if CHIP_USED == 1
-    {
-        //Set V0 pin
-        GPIOPinSet(HD4470_V0_PORT, HD4470_V0_PIN);
-        //Set RS pin
-        GPIOPinSet(HD4470_RS_PORT, HD4470_RS_PIN);
-        //Set RW pin
-        GPIOPinSet(HD4470_RW_PORT, HD4470_RW_PIN);
-        //Set E pin
-        GPIOPinSet(HD4470_E_PORT, HD4470_E_PIN);
-
-        //Set D4
-        GPIOPinSet(HD4470_DATA_4_PORT, HD4470_DATA_4_PIN);
-        //Set D5
-        GPIOPinSet(HD4470_DATA_5_PORT, HD4470_DATA_5_PIN);
-        //Set D6
-        GPIOPinSet(HD4470_DATA_6_PORT, HD4470_DATA_6_PIN);
-        //Set D7
-        GPIOPinSet(HD4470_DATA_7_PORT, HD4470_DATA_7_PIN);
-    }
-    #else if CHIP_USED == 2
-    {
-        //DO NOTHING
-    }
-    #else
-    {
-        //DO NOTHING
-    }
-    #endif
-}
-
-static void lcd_delay_ms(uint32_t ms) 
-{
-    // Implement delay
 }
