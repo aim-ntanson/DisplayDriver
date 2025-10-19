d397997dd43e9cbcc1f419315d88cd9b1ab9ce8f
TamNgo <ngotam1504@gmail.com>
2025-10-19 13:29:53 +0700

    [feat] Add lcd_write, build passed

--- Source/LCD1602_HD44780/lcd1602_hd4470.h
+++ Source/LCD1602_HD44780/lcd1602_hd4470.h
@@ -1,9 +1,10 @@
-#ifndef LCD1602_HD4470_H
-#define LCD1602_HD4470_H
+#ifndef __LCD1602_HD4470_H__
+#define __LCD1602_HD4470_H__
 
 #include <stdint.h>
 
 #include "./../display_wrapper.h"
+#include "./../chipset_wrapper.h"
 
 // LCD1602 PIN configuration
 #define HD4470_V0_PORT      WRAPPER_V0_PORT  // Register Select pin
@@ -98,7 +99,7 @@
 // Function prototypes
 void lcd1602_init(void);
 static void lcd1602_clear(void);
-static void lcd_write_command(uint8_t cmd);
+static void lcd_write_command(uint8_t cmd_data);
 static void lcd_write_data(uint8_t data);
 static void lcd_delay_ms(uint32_t ms);
 static void falling_edge(void);
